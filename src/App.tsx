2434ade61de09698571d9a1545fe2c7f7fcde6fc
Trevor Pierce <yeahtrevorpierce@gmail.com>
2022-07-24 13:46:05 -0400

    Updated app to set theme based on user system preference (light mode/dark mode)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
+import { useMemo } from "react";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import { TopicSidebar } from "./components/TopicSidebar";
 
 function App() {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? "dark" : "light",
+        },
+      }),
+    [prefersDarkMode]
+  );
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <TopicSidebar />
-    </>
+    </ThemeProvider>
   );
 }
 
