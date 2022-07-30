3fa80c420d6f7b50411e108bc4719a84bbcae6e2
Trevor Pierce <yeahtrevorpierce@gmail.com>
2022-07-29 23:06:49 -0400

    Updating to have toggleable sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
+import styles from "./App.module.css";
 import { TopicSidebar } from "./components/TopicSidebar";
 
 const App = () => {
+  const [sidebarIsVisible, setSidebarVisibility] = useState(true);
   return (
-    <>
-      <TopicSidebar />
-    </>
+    <div className={styles.appContainer}>
+      {sidebarIsVisible && <TopicSidebar />}
+      <main>
+        <button
+          type="button"
+          onClick={() => setSidebarVisibility(!sidebarIsVisible)}
+        >
+          {sidebarIsVisible ? "Hide Sidebar" : "Show Sidebar"}
+        </button>
+      </main>
+    </div>
   );
 };
 
