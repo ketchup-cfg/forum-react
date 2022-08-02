135e253d4c7374592b1179a129f48728366e0527
Trevor Pierce <yeahtrevorpierce@gmail.com>
2022-08-01 20:34:01 -0400

    Made sidebar close when clicked outside of topic sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,32 @@ import { TopicSidebar } from "./components/TopicSidebar";
 
 const App = () => {
   const [sidebarIsVisible, setSidebarVisibility] = useState(false);
+
+  const handleAppClick = (event: React.MouseEvent) => {
+    const topicSidebar = document.querySelector("#topicSidebar");
+    const topicSidebarButton = document.querySelector("#topicSidebarButton");
+
+    if (
+      !topicSidebar?.contains(event.target as Node) &&
+      !topicSidebarButton?.contains(event.target as Node)
+    ) {
+      setSidebarVisibility(false);
+    }
+  };
+
   return (
-    <div className={styles.appContainer}>
-      <div>{sidebarIsVisible && <TopicSidebar />}</div>
+    <div className={styles.appContainer} onClick={handleAppClick}>
+      <div id="topicSidebar">{sidebarIsVisible && <TopicSidebar />}</div>
       <div className={styles.appCanvas}>
         <button
           className={styles.topicSidebarButton}
           type="button"
+          id="topicSidebarButton"
           onClick={() => setSidebarVisibility(!sidebarIsVisible)}
         >
           <i className={`fa-solid fa-bars ${styles.icons}`}></i>
         </button>
-        <main></main>
+        <main className={styles.mainWrapper}></main>
         <button
           className={styles.topicSidebarButton}
           type="button"
