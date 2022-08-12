651eb1f42183e788032b7d2a42978368df4bff11
Trevor Pierce <yeahtrevorpierce@gmail.com>
2022-08-11 21:35:47 -0400

    Added unit tests for when topic button is pressed

--- src/__tests__/App-test.js
+++ src/__tests__/App-test.js
@@ -1,5 +1,5 @@
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+import { render, fireEvent, screen } from "@testing-library/react";
 import App from "../App";
 
 describe("When the application is initially rendered...", () => {
@@ -19,5 +19,46 @@ describe("When the application is initially rendered...", () => {
     render(<App />);
 
     expect(screen.queryByTestId("app-content")).toHaveClass("ml-0");
+  });
+});
+
+describe("When the topic button in the navbar is clicked...", () => {
+  test("The topics button is still visible", async () => {
+    render(<App />);
+    fireEvent(
+      screen.getByText("Topics"),
+      new MouseEvent("click", {
+        bubbles: true,
+        cancelable: true,
+      })
+    );
+
+    expect(screen.getByText("Topics")).toBeVisible();
+  });
+
+  test("The topic sidebar is display", async () => {
+    render(<App />);
+    fireEvent(
+      screen.getByText("Topics"),
+      new MouseEvent("click", {
+        bubbles: true,
+        cancelable: true,
+      })
+    );
+
+    expect(screen.queryByText("Create New Topic")).toBeVisible();
+  });
+
+  test("The navbar and main content have the ml-64 class", async () => {
+    render(<App />);
+    fireEvent(
+      screen.getByText("Topics"),
+      new MouseEvent("click", {
+        bubbles: true,
+        cancelable: true,
+      })
+    );
+
+    expect(screen.queryByTestId("app-content")).toHaveClass("ml-64");
   });
 });
