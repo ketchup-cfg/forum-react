4d37bf516d20e25e7db82452e4424ccca79eff64
Trevor Pierce <yeahtrevorpierce@gmail.com>
2022-08-12 10:31:15 -0400

    Increased code coverage to cover more situations in App.tsx

--- src/__tests__/App-test.js
+++ src/__tests__/App-test.js
@@ -186,3 +186,44 @@ describe("When the topic button in the navbar is clicked and then the main app c
     expect(screen.queryByTestId("app-main-screeen")).toHaveClass("ml-0");
   });
 });
+
+describe("When the application is initially rendered and then the main app content section is clicked...", () => {
+  test("The topics button is visible", async () => {
+    render(<App />);
+    fireEvent(
+      screen.queryByTestId("app-main-content"),
+      new MouseEvent("click", {
+        bubbles: true,
+        cancelable: true,
+      })
+    );
+
+    expect(screen.queryByText("Topics")).toBeVisible();
+  });
+
+  test("The topic sidebar is not present", async () => {
+    render(<App />);
+    fireEvent(
+      screen.queryByTestId("app-main-content"),
+      new MouseEvent("click", {
+        bubbles: true,
+        cancelable: true,
+      })
+    );
+
+    expect(screen.queryByText("Create New Topic")).not.toBeInTheDocument();
+  });
+
+  test("The navbar and main content have the ml-0 class", async () => {
+    render(<App />);
+    fireEvent(
+      screen.queryByTestId("app-main-content"),
+      new MouseEvent("click", {
+        bubbles: true,
+        cancelable: true,
+      })
+    );
+
+    expect(screen.queryByTestId("app-main-screeen")).toHaveClass("ml-0");
+  });
+});
