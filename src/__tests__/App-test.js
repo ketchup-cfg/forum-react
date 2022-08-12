680de3f83f337205fa629ecb7e25a6ec091d7de8
Trevor Pierce <yeahtrevorpierce@gmail.com>
2022-08-12 10:43:54 -0400

    100% test coverage for App.tsx

--- src/__tests__/App-test.js
+++ src/__tests__/App-test.js
@@ -227,3 +227,65 @@ describe("When the application is initially rendered and then the main app conte
     expect(screen.queryByTestId("app-main-screeen")).toHaveClass("ml-0");
   });
 });
+
+describe("When the topic button in the navbar is clicked and then close button in the topic sidebar is clicked...", () => {
+  test("The topics button is visible", async () => {
+    render(<App />);
+    fireEvent(
+      screen.queryByText("Topics"),
+      new MouseEvent("click", {
+        bubbles: true,
+        cancelable: true,
+      })
+    );
+    fireEvent(
+      screen.queryByText("×"),
+      new MouseEvent("click", {
+        bubbles: true,
+        cancelable: true,
+      })
+    );
+
+    expect(screen.queryByText("Topics")).toBeVisible();
+  });
+
+  test("The topic sidebar is not present", async () => {
+    render(<App />);
+    fireEvent(
+      screen.queryByText("Topics"),
+      new MouseEvent("click", {
+        bubbles: true,
+        cancelable: true,
+      })
+    );
+    fireEvent(
+      screen.queryByText("×"),
+      new MouseEvent("click", {
+        bubbles: true,
+        cancelable: true,
+      })
+    );
+
+    expect(screen.queryByText("Create New Topic")).not.toBeInTheDocument();
+  });
+
+  test("The navbar and main content have the ml-0 class", async () => {
+    render(<App />);
+    fireEvent(
+      screen.queryByText("Topics"),
+      new MouseEvent("click", {
+        bubbles: true,
+        cancelable: true,
+      })
+    );
+    fireEvent(
+      screen.queryByText("×"),
+      new MouseEvent("click", {
+        bubbles: true,
+        cancelable: true,
+      })
+    );
+
+    expect(screen.queryByTestId("app-main-screeen")).toHaveClass("ml-0");
+  });
+});
